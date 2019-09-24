c0243694a814568a72625aa28309522b5b2e3d4c
Sebi <sebichiotoroiu@yahoo.com>
2019-09-24 13:59:57 +0200

    BasicSebi/

---
+++ BasicSebi/basic1.js
@@ -0,0 +1,34 @@
+//
+var random = Math.floor((Math.random()*25)-5);
+
+console.log(random);
+
+var i = random;
+if (i = -5 && i< 10) {
+  document.write("The weather is cold:")
+}
+else{
+document.write("The weather is moderate:");
+}//Basic1//
+
+
+console.log(Math.max(1, 7, -3, 9)); //Basic2//
+console.log(Math.min(1, 7, -3, 9));
+
+
+function multiplyBy(){
+        }
+console.log(multiplyBy([1,3,7,10,2]));
+
+//
+document.write("<br>");
+var random = Math.floor((Math.random()*45)-5);
+console.log(random);
+var i = random;
+if (32 < i) {
+  document.write("The weather is cold:")
+}
+else{
+document.write("The weather is moderate:");
+}
+return (random);
